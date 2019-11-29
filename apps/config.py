ac3ead09d776bdc67e980744102d19c2319d0a22
feelingslw <wei147852>
2019-11-29 17:50:17 +0800

    init db

--- apps/config.py
+++ apps/config.py
@@ -7,5 +7,4 @@ HOST = '192.168.52.53'
 PORT = 3306
 DATABASE = 'flask'
 
-SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST,
-                                                                          PORT,
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST,PORT,DATABASE)
